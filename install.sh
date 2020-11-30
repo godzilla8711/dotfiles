0755fdb8a2a053c2fb023a92c22062e20dd89edd
Tony Cetuk <cetukt@gmail.com>
2020-11-30 01:16:55 -0500

    add install/uninstall scripts

--- install.sh
+++ install.sh
@@ -1,3 +1,19 @@
 # Install fzf binary
 ./bin/fzf/install
 
+# Install bat
+sudo dpkg -i bin/bat_0.17.1_amd64.deb
+
+#!/bin/bash
+# Fetch all patches for the OS 
+yum update -y
+# Install GIT, NGINX and NODE/NPM
+yum install git -y
+yum install nginx -y
+curl --silent --location https://rpm.nodesource.com/setup_8.x | bash -
+yum install nodejs -y
+# Fetch the script that will install the s/w repos 
+cd /home/ec2-user
+wget https://raw.githubusercontent.com/godzilla8711/akira-docs/master/init.sh 
+chmod 777 /home/ec2-user/init.sh
+
---
+++ uninstall.sh
@@ -0,0 +1,6 @@
+# Uninstall fzf binary
+./bin/fzf/uninstall
+
+# Uninstall bat
+sudo dpkg -r bat
+
